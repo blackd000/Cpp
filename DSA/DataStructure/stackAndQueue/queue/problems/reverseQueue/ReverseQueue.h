c868fe12829e9de5e21218c19c5aaada2c7f1c77
blackd000 <dongminhcho89@gmail.com>
2022-10-31 10:25:11 +0700

    add

--- DSA/DataStructure/stackAndQueue/queue/problems/reverseQueue/ReverseQueue.h
+++ DSA/DataStructure/stackAndQueue/queue/problems/reverseQueue/ReverseQueue.h
@@ -5,6 +5,8 @@
 #include "../linkedListQueue/LinkedListQueue.h"
 #include "../../stack/linkedListStack/LinkedListStack.h"
 
+// - Time Complexity: O(n). 
+// - Space Complexity: O(n)
 void reverseQueueFirstKElement(int k, Queue *queue);
 
 #endif
